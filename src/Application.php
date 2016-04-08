b4e0e983378dfdb6f2b8a2fc43eb2b5c2553360d
mucean <mucean@163.com>
2016-04-09 00:20:51 +0800

    complete Application::registerNamespace function

--- src/Application.php
+++ src/Application.php
@@ -9,7 +9,7 @@ class Application
         //todo start
     }
 
-    public function registerNamespace($dir, callable $func = null)
+    public function registerNamespace($dir, $namespace, callable $func = null)
     {
         $dir = rtrim(strval($dir), DIRECTORY_SEPARATOR);
         if (DEBUG) {
@@ -17,5 +17,28 @@ class Application
                 throw new \Exception(sprintf('invalid directory was given: %s', $dir));
             }
         }
+
+        if ($func === null) {
+            $pre_namespace = ltrim($namespace, '\\');
+            $pre_namespace_len = strlen($pre_namespace);
+            $func = function ($class) use ($pre_namespace, $dir, $pre_namespace_len) {
+                if ($pre_namespace === '') {
+                    $part_dir = str_replace('\\', '/', ltrim($class, '\\'));
+                } else {
+                    $class = ltrim($class, '\\');
+                    $validate_result = strpos($class, $pre_namespace);
+                    if ($validate_result === false || $validate_result > 0) {
+                        return null;
+                    }
+                    $part_dir = str_replace('\\', '/', substr($class, $pre_namespace_len + 1));
+                }
+                $file = $dir . DIRECTORY_SEPARATOR . $part_dir . '.php';
+                if (is_file($file)) {
+                    require_once($file);
+                }
+            };
+        }
+
+        spl_autoload_register($func);
     }
 }

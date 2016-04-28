e48816302feb8c42ab790dbb580f1da29a09bc5b
mucean <mucean@163.com>
2016-04-28 09:56:54 +0800

    complete getRequestUri function

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -4,22 +4,91 @@ namespace Etu\Http;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
+use Etu\Http\Uri;
 
 class Request extends ServerRequestInterface
 {
     use MessageTrait;
 
     protected $servers = [];
+    protected $cookies = [];
 
     protected $uri = null;
 
     public function __construct()
     {
         $this->servers = $_SERVER;
+        $this->cookies = $_COOKIE;
         $this->setHeaders(getallheaders());
         if (!$this->hasHeader('host') && isset($_SERVER['SERVER_NAME'])) {
             $this->withHeader('Host', $_SERVER['SERVER_NAME']);
         }
+    }
+
+    public function getServerParams()
+    {
+        return $this->servers;
+    }
+
+    public function getCookieParams()
+    {
+        return $this->cookies;
+    }
+
+    public function withCookieParams(array $cookies)
+    {
+        if ($this->cookies === $cookies) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->cookies = $cookies;
+        return $new;
+    }
+
+    public function getQueryParams()
+    {
+        if (isset($this->servers['QUERY_STRING'])) {
+            return $this->servers['QUERY_STRING'];
+        }
+        if ($this->uri === null) {
+            $this->uri = $this->getRequestUri();
+        }
+
+        return $this->uri->getQuery();
+    }
+
+    public function getRequestUri()
+    {
+        $uri = new Uri;
+        $servers = &$this->servers;
+        $uri->withScheme(!empty($servers['HTTPS'] && $servers['HTTPS'] == 'on' ? 'https' : 'http'));
+        $uri->withHost(!empty($servers['SERVER_NAME'])
+            ? $servers['SERVER_NAME']
+            : $this->hasHeader('host') ? $this->getHeader('host') : '');
+        $uri->withPort(!empty($servers['SERVER_PORT']) ? $servers['SERVER_PORT'] : null);
+
+        $path = $query = '';
+        if (!empty($servers['REQUEST_URI'])) {
+            $requestUri = $servers['REQUEST_URI'];
+            if (($pos = strpos($requestUri, '?')) !== false) {
+                $path = substr($requestUri, 0, $pos);
+                $query = substr($requestUri, $pos + 1);
+            } else {
+                $path = $requestUri;
+                $query = '';
+            }
+        } else {
+            $path = $servers['PHP_SELF'];
+            if (isset($servers['argv'])) {
+                $query = $servers['argv'][0];
+            } elseif (isset($servers['QUERY_STRING'])) {
+                $query = $servers['QUERY_STRING'];
+            }
+        }
+        $uri->withPath($path);
+        $uri->withQuery($query);
+        return $uri;
     }
 
     public function withUri(UriInterface $uri, $preserveHost = false)
@@ -33,7 +102,7 @@ class Request extends ServerRequestInterface
         $host = $uri->getHost();
         if (!$preserveHost && $host) {
             if ($port = $uri->getPort()) {
-                $host = $host . $port;
+                $host .= ':' . $port;
             }
             $this->withHeader('Host', $host);
         }
--- src/Http/Uri.php
+++ src/Http/Uri.php
@@ -6,6 +6,13 @@ use Psr\Http\Message\UriInterface;
 
 class Uri implements UriInterface
 {
+    protected static $charsUnreserved = 'a-zA-Z0-9\-\._~';
+    protected static $charsSubDelim = '!\$&\'\(\)\*\+,;=';
+    protected static $standardPort = [
+        'http' => 80,
+        'https' => 443
+    ];
+
     protected $scheme = false;
     protected $host = false;
     protected $port = false;
@@ -14,34 +21,27 @@ class Uri implements UriInterface
     protected $path = false;
     protected $query = false;
     protected $fragment = false;
-    protected $component = false;
 
-    protected static $standardPort = [
-        'http' => 80,
-        'https' => 443
-    ];
-
-    public function __construct($url)
+    public function __construct($url = null)
     {
-        $this->component = parse_url($url);
-        if ($this->component === false) {
+        if ($url === null) {
+            return $this;
+        }
+        $component = parse_url($url);
+        if ($component === false) {
             throw new \Exception('Class Uri construct with a valid url');
         }
+        $this->applyComponent($component);
     }
 
     public function getScheme()
     {
-        if ($this->scheme === false) {
-            $this->scheme = isset($this->component['scheme'])
-                ? $this->component['scheme']
-                : '';
-        }
         return $this->scheme;
     }
 
     public function getAuthority()
     {
-        if (empty($this->getHost())) {
+        if (empty($this->host)) {
             return '';
         }
 
@@ -50,7 +50,7 @@ class Uri implements UriInterface
             $authority = $userInfo . '@' . $this->host;
         }
 
-        if ($this->getPort() !== null && $this->validatePort($this->scheme, $this->host, $this->port)) {
+        if ($this->port !== null && $this->validatePort($this->scheme, $this->host, $this->port)) {
             $authority .= ':' . $this->port;
         }
 
@@ -59,15 +59,6 @@ class Uri implements UriInterface
 
     public function getUserInfo()
     {
-        if ($this->user === false) {
-            $this->user = isset($this->component['user'])
-                ? $this->component['user']
-                : '';
-            $this->pass = isset($this->component['pass'])
-                ? $this->component['pass']
-                : '';
-        }
-
         if (empty($this->user)) {
             return '';
         }
@@ -83,40 +74,183 @@ class Uri implements UriInterface
 
     public function getHost()
     {
-        if ($this->host === false) {
-            $this->host = isset($this->component['host'])
-                ? strtolower($this->component['host'])
-                : '';
-        }
         return $this->host;
     }
 
     public function getPort()
     {
-        if ($this->port === false) {
-            $this->port = isset($this->component['port'])
-                ? $this->normalizePort($this->scheme, $this->host, $this->component['port'])
-                : null;
-        }
         return $this->port;
     }
 
     public function getPath()
     {
-        if ($this->path === false) {
-            $this->path = isset($this->component['path'])
-                ? $this->component['path']
-                : '';
-        }
+        return $this->path;
     }
 
-    protected function applyComponent()
+    public function getQuery()
     {
-        $this->query = isset($this->component['query'])
-            ? $this->component['query']
+        return $this->query;
+    }
+
+    public function getFragment()
+    {
+        return $this->fragment;
+    }
+
+    public function withScheme($scheme)
+    {
+        $scheme = trim(strtolower($scheme));
+        if ($scheme === $this->scheme) {
+            return $this;
+        }
+        $this->scheme = $scheme;
+        $this->port = $this->normalizePort($scheme, $this->host, $this->port);
+        return $this;
+    }
+
+    public function withUserInfo($user, $password = null)
+    {
+        $this->user = $user;
+        if ($password !== null) {
+            $this->pass = $password;
+        }
+
+        return $this;
+    }
+
+    public function withHost($host)
+    {
+        $this->host = strtolower($host);
+    }
+
+    public function withPort($port)
+    {
+        $this->port = $this->normalizePort($this->scheme, $this->host, $port);
+        return $this;
+    }
+
+    public function withPath($path)
+    {
+        if (DEBUG) {
+            if (!is_string($path) && !method_exists($path, '__toString')) {
+                throw new \InvalidArgumentException(
+                    'path argument must be a string'
+                );
+            }
+        }
+        $this->path = $this->normalizePath($path);
+        return $this;
+    }
+
+    public function withQuery($query)
+    {
+        if (DEBUG) {
+            if (!is_string($query) && !method_exists($query, '__toString')) {
+                throw new \InvalidArgumentException(
+                    'query argument must be a string'
+                );
+            }
+        }
+        if (strpos($query, '?') === 0) {
+            $query = substr($query, 1);
+        }
+        $this->query = $this->normalizeQueryAndFragment($query);
+        return $this;
+    }
+
+    public function withFragment($fragment)
+    {
+        if (DEBUG) {
+            if (!is_string($fragment) && !method_exists($fragment, '__toString')) {
+                throw new \InvalidArgumentException(
+                    'fragment argument must be a string'
+                );
+            }
+        }
+        if (strpos($fragment, '#') === 0) {
+            $fragment = substr($fragment, 1);
+        }
+
+        $this->fragment = $this->normalizeQueryAndFragment($fragment);
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->createUriString(
+            $this->scheme,
+            $this->getAuthority(),
+            $this->path,
+            $this->query,
+            $this->fragment
+        );
+    }
+
+    /**
+     * Create a URI string from its various parts
+     *
+     * @param string $scheme
+     * @param string $authority
+     * @param string $path
+     * @param string $query
+     * @param string $fragment
+     * @return string
+     */
+    private function createUriString($scheme, $authority, $path, $query, $fragment)
+    {
+        $uri = '';
+
+        if (!empty($scheme)) {
+            $uri .= $scheme . '://';
+        }
+
+        if (!empty($authority)) {
+            $uri .= $authority;
+        }
+
+        if ($path != null) {
+            if ($uri && substr($path, 0, 1) !== '/') {
+                $uri .= '/';
+            }
+            $uri .= $path;
+        }
+
+        if ($query != null) {
+            $uri .= '?' . $query;
+        }
+
+        if ($fragment != null) {
+            $uri .= '#' . $fragment;
+        }
+
+        return $uri;
+    }
+
+    protected function applyComponent(&$component)
+    {
+        $this->scheme = isset($component['scheme'])
+            ? $component['scheme']
             : '';
-        $this->fragment = isset($this->component['fragment'])
-            ? $this->component['fragment']
+        $this->user = isset($component['user'])
+            ? $component['user']
+            : '';
+        $this->pass = isset($component['pass'])
+            ? $component['pass']
+            : '';
+        $this->host = isset($component['host'])
+            ? strtolower($component['host'])
+            : '';
+        $this->port = isset($component['port'])
+            ? $this->normalizePort($this->scheme, $this->host, $component['port'])
+            : null;
+        $this->path = isset($component['path'])
+            ? $this->normalizePath($component['path'])
+            : '';
+        $this->query = isset($component['query'])
+            ? $this->normalizeQueryAndFragment($component['query'])
+            : '';
+        $this->fragment = isset($component['fragment'])
+            ? $this->normalizeQueryAndFragment($component['fragment'])
             : '';
     }
 
@@ -135,6 +269,22 @@ class Uri implements UriInterface
         }
 
         return $this->validatePort($scheme, $host, $port) ? $port : null;
+    }
+
+    protected function normalizePath($path)
+    {
+        $preg = '/(?:[^' . self::$charsUnreserved . self::$charsSubDelim . ':@\/%]+|%(?![a-zA-Z0-9]{2}))/';
+        return preg_replace_callback($preg, function ($matches) {
+            return rawurlencode($matches[0]);
+        }, $path);
+    }
+
+    protected function normalizeQueryAndFragment($str)
+    {
+        $preg = '/(?:[^' . self::$charsUnreserved . self::$charsSubDelim . ':@\/%\?]+|%(?![A-Fa-f0-9]{2}))/';
+        return preg_replace_callback($preg, function ($matches) {
+            return rawurlencode($matches[0]);
+        }, $str);
     }
 
     protected function validatePort($scheme, $host, $port)

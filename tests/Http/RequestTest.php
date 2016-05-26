55af77139e6bc0878016666cb62a068b1abb64ae
mucean <moceanliu@163.com>
2016-05-26 21:04:58 +0800

    add Request class test

--- tests/Http/RequestTest.php
+++ tests/Http/RequestTest.php
@@ -6,4 +6,29 @@ use Etu\Http\Request;
 
 class RequestTest extends \PHPUnit_Framework_TestCase
 {
+    public function testBuildFromContext()
+    {
+        $context = BuildContext::getContext();
+        $request = Request::buildFromContext($context);
+        $this->assertInstanceOf('Etu\Http\Request', $request);
+        return $request;
+    }
+
+    /**
+     * @depends testBuildFromContext
+     */
+    public function testGetServerParams(Request $request)
+    {
+        $originalServer = BuildContext::$context;
+        $this->assertEquals($originalServer, $request->getServerParams());
+    }
+
+    /**
+     * @depends testBuildFromContext
+     */
+    public function testGetCookieParams(Request $request)
+    {
+        $this->assertEquals($_COOKIE, $request->getCookieParams());
+        $this->assertEquals([], $request->getCookieParams());
+    }
 }

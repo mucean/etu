d38224eac127f7b1d38bf472037e2eba2bdf5966
mucean <moceanliu@163.com>
2016-06-02 18:31:51 +0800

    start write response

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -2,6 +2,15 @@
 
 namespace Etu\Response;
 
-class Response
+use Psr\Http\Message\ResponseInterface;
+use InvalidArgumentException;
+
+class Response extends Message implements ResponseInterface
 {
+    protected $statusCode = 200;
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
 }

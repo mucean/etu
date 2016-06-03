40109cb0177a3b6fb737335504e181e87dc67866
mucean <mucean@163.com>
2016-06-03 08:52:44 +0800

    complete Response class and tests

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -1,16 +1,156 @@
 <?php
 
-namespace Etu\Response;
+namespace Etu\Http;
 
 use Psr\Http\Message\ResponseInterface;
 use InvalidArgumentException;
+use Psr\Http\Message\StreamInterface;
+use Etu\Stream;
 
 class Response extends Message implements ResponseInterface
 {
     protected $statusCode = 200;
 
+    protected $reasonPhrase;
+
+    protected static $validCodeAndReasonMap = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unassigned',
+        426 => 'Upgrade Required',
+        427 => 'Unassigned',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        430 => 'Unassigned',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Unassigned',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+    ];
+
+    public function __construct($statusCode = 200, array $headers = [], StreamInterface $body = null)
+    {
+        $this->statusCode = $statusCode;
+        $this->setHeaders($headers);
+        $this->body = $body ? $body : new Stream(fopen('php://temp', 'w+'));
+    }
+
     public function getStatusCode()
     {
         return $this->statusCode;
+    }
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        $this->validateStatusCode($code);
+        $new = clone $this;
+        $new->statusCode = $code;
+        if ($reasonPhrase) {
+            $new->reasonPhrase = $reasonPhrase;
+        } else {
+            $new->reasonPhrase = $new->getDefaultReasonPhrase($code);
+        }
+
+        return $new;
+    }
+
+    public function getReasonPhrase()
+    {
+        if ($this->reasonPhrase !== null) {
+            return $this->reasonPhrase;
+        }
+
+        $statusCode = $this->getStatusCode();
+
+        return $this->reasonPhrase = $this->getDefaultReasonPhrase($statusCode);
+    }
+
+    public function write($data)
+    {
+        $this->getBody()->write($data);
+        return $this;
+    }
+
+    public function redirect($url, $statusCode = 302)
+    {
+        $new = $this->withHeader('Location', (string) $url);
+
+        if ($new->getStatusCode() === 200) {
+            return $new->withStatus($statusCode);
+        }
+
+        return $new;
+    }
+
+    protected function validateStatusCode($statusCode)
+    {
+        if (!is_integer($statusCode) || $statusCode < 100 || $statusCode > 599) {
+            throw new InvalidArgumentException('Invalid status code');
+        }
+    }
+
+    protected function getDefaultReasonPhrase($statusCode)
+    {
+        if (isset(static::$validCodeAndReasonMap[$statusCode])) {
+            return static::$validCodeAndReasonMap[$statusCode];
+        } else {
+            return '';
+        }
     }
 }
---
+++ tests/Http/ResponseTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Http;
+
+use Etu\Http\Response;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $response = new Response();
+        $this->assertInstanceOf('Etu\Http\Response', $response);
+        return $response;
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testGetStatusCode(Response $response)
+    {
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function getReasonPhrase(Response $response)
+    {
+        $this->assertEquals($response->getReasonPhrase(), 'OK');
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testWithStatus(Response $response)
+    {
+        $newResponse = $response->withStatus(404, 'hello, world!');
+        $this->assertEquals($newResponse->getStatusCode(), 404);
+        $this->assertEquals($newResponse->getReasonPhrase(), 'hello, world!');
+        $newResponse = $response->withStatus(404);
+        $this->assertEquals($newResponse->getReasonPhrase(), 'Not Found');
+        $newResponse = $response->withStatus(555);
+        $this->assertEquals($newResponse->getReasonPhrase(), '');
+        $this->setExpectedException('InvalidArgumentException', 'Invalid status code');
+        $response->withStatus(900);
+    }
+}
